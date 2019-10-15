d9c82bead9deb1f109ffccab7fd77ed628852537
joeyHarig <55154968+joeyHarig@users.noreply.github.com>
2019-10-15 10:31:06 -0600

    Analytics

--- analytics.js
+++ analytics.js
@@ -2,3 +2,13 @@ window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'UA-149507678-1');
+
+document.addEventListener('click', function(event) {
+    if(event.target.name != null) {
+        ga('send', {
+            hitType: 'event',
+            eventCategory: event.target.name,
+            eventAction: 'click'
+        });
+    }
+});
